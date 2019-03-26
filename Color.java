de3e5b65fc62f81b46eb7d9c2e89fbef31f5ab14
Dillan Gibbons <Dillan.G.Gibbons-1@ou.edu>
2019-03-26 15:37:07 -0500

    Created all methods and enum constants

---
+++ Color.java
@@ -0,0 +1,24 @@
+
+public enum Color {
+	RED(255, 0, 0), GREEN(0, 255, 0), BLUE(0, 0, 255), YELLOW(255, 255, 0), CYAN(0, 255, 255), MAGENTA(255, 0, 255);
+	private int r;
+	private int b;
+	private int g;
+	
+		
+
+	private Color(int r, int g, int b) {
+		this.r = r;
+		this.b = b;
+		this.g = g;
+	}
+	public int getR() {
+		return this.r;
+	}
+	public int getB() {
+		return this.b;
+	}
+	public int getG() {
+		return this.g;
+	}
+}
